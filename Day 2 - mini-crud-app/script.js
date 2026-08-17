de2024cf9eb7de563d85bcfd15316692eefc9551
Joseph Ramos <202311330@gordoncollege.edu.ph>
2026-08-17 23:07:03 +0800

    fixing error in database connection

--- Day 2 - mini-crud-app/script.js
+++ Day 2 - mini-crud-app/script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://brxmhlkdohphskgvacix.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://brxmhlkdohphskgvacix.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyeG1obGtkb2hwaHNrZ3ZhY2l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NzEwMzAsImV4cCI6MjEwMjU0NzAzMH0.oRNktFkVLN6Qlc3X0icmeg4w-sgfhXnhJK-yfMpHncE';
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -16,7 +16,7 @@ async function createNote() {
     }
 
     const { error } = await _supabase
-        .from('notes');
+        .from('notes')
         .insert([{content: text}]);
 
     if (error) {
